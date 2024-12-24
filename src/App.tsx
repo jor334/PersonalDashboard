986eabdc050e42f24a9edea49f861e7973e90645
jor334 <j.ortizr@uniandes.edu.co>
2024-12-23 21:59:30 -0500

    some fixes of unnecesary code

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/header';
 import Dashboard from './components/dashboard';
 import Footer from './components/footer';
-import Pets from './components/pets';
+
 
 const App = () => {
     return (
