de64964a212ebf4a5d765ffe6e65e348a91c03da
jor334 <j.ortizr@uniandes.edu.co>
2024-12-23 22:21:00 -0500

    correccion de escritura menor (mala pa jemos)

--- src/components/dashBoard.tsx
+++ src/components/dashBoard.tsx
@@ -9,7 +9,7 @@ export default function DashBoard(){
             <div className="grid grid-cols-2 gap-4 ">
                 <Cards title='Spotify' description='last played songs!' image='src/assets/spotify-icon.svg' url='https://www.last.fm/user/JJor334'></Cards>
                 <Cards title='Steam' description='descripcion steam' image='src/assets/steam-icon-logo.svg' url='https://steamcommunity.com/profiles/76561198359795971'></Cards>
-                <Cards title='Portoflio (in progress)' description='web and mobile porfolio in progress!' image='src/assets/visual-studio-code-1.svg' url=''></Cards>
+                <Cards title='Portflio (in progress)' description='web and mobile portfolio in progress!' image='src/assets/visual-studio-code-1.svg' url=''></Cards>
                 <Cards title='github account' description='follow my github account!' image='src/assets/github-icon-2.svg' url='https://github.com/jor334'></Cards>  
                 
             </div> 
